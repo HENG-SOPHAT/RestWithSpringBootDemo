ca3ef2b75561c82008f8263a013582de2471d043
sophat <sophat.heng@asiacell.com>
2020-04-11 14:35:21 +0300

    My test spring boot project

---
+++ src/main/java/com/example/restspringbootdemo/RestSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.restspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restspringbootdemo/RestSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
